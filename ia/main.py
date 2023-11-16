76e56e9e7c56522d458c07037dc2e71236899fc6
AymaraQC <qcaymara18@gmail.com>
2023-11-16 20:11:35 -0300

    edite cambios en el diseño de la pagina y agregue seeds

--- ia/main.py
+++ ia/main.py
@@ -77,6 +77,81 @@ def main():
             "title": "Resumen de la serie megalodon",
             "description": "el mejor resumen de la serie megalodon",
         },
+
+        {
+           "title": "Los avances más recientes en inteligencia artificial y su impacto en la sociedad",
+            "description": "avances en IA y su impacto", 
+        },
+
+        {
+            "title": "Cómo cultivar un jardín de hierbas en espacios pequeños",
+            "description": "cultivar jardín de hierbas en espacios reducidos",
+        },
+
+        {
+            "title": "Entrevista exclusiva con un astronauta: La vida en el espacio",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Reseña crítica de la última película de ciencia ficción",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Guía paso a paso para mejorar la productividad en el trabajo",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Los beneficios del yoga en la salud mental y física",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Perfil de éxito: La historia detrás del emprendimiento de una startup",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "10 consejos esenciales para viajar de manera económica",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Análisis de las tendencias de moda para la temporada otoño-invierno",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Cómo elegir la mejor cámara fotográfica para principiantes",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Investigación académica: Impacto del cambio climático en los ecosistemas marinos",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Crónica de un evento cultural: Festival de Arte y Música 2023",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Crónica de un evento cultural: Festival de Arte y Música 2023",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Humor satírico: Las situaciones más absurdas en la vida cotidiana",
+            "description": "el mejor resumen de la serie megalodon",
+        },
+
+        {
+            "title": "Los secretos detrás de una dieta balanceada y sus efectos en la salud",
+            "description": "el mejor resumen de la serie megalodon",
+        },
     ]
 
     for seed in seeds:
